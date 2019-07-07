588c221d14bb4b728f5b88978eca5a4114687bec
Roshan Kumaraswamy <roshie548@gmail.com>
2019-07-07 12:21:34 -0700

    Updates

---
+++ precache-manifest.aa14aec359830e5b52fc9a7972ac4650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc6a550425eeeca41cc78b656c30ff6",
+    "url": "/MyPortfolioSite/index.html"
+  },
+  {
+    "revision": "957e4bb4b7689ddd9a6d",
+    "url": "/MyPortfolioSite/static/css/main.d2d365e9.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/MyPortfolioSite/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "957e4bb4b7689ddd9a6d",
+    "url": "/MyPortfolioSite/static/js/main.962d030f.chunk.js"
+  },
+  {
+    "revision": "c9e37abbd3ee9be7a4fa",
+    "url": "/MyPortfolioSite/static/js/runtime~main.77357a2f.js"
+  },
+  {
+    "revision": "08baf6cf49772ff8627f59f6e32fc954",
+    "url": "/MyPortfolioSite/static/media/avatar.08baf6cf.png"
+  }
+]);
